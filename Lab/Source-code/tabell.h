3bc48f2fab955156293634d4bcc88d81936c16b0
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-03-29 13:25:01 +0200

    added tabell files
    These are the files for the tabells.

---
+++ Lab/Source-code/tabell.h
@@ -0,0 +1,13 @@
+/* tabell.h */
+/* Developer MissPink */
+
+#include "ranta.h"
+#include <stdio.h>
+
+#if !defined(tabell_h)
+	#define tabell_h
+
+   #define	ABS( x )	( (x) > 0 ? (x) : -(x) )
+	void TabellPaSkarmen( double kapital, int antalAr );
+
+#endif
