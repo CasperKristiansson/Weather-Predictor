6735dada20fbf6779040107b0219b901729f0d0a
Casper kristiansson <Casper.kristiansson@yahoo.se>
2022-04-08 13:18:26 +0200

    Add data_management class

---
+++ Backend/Library/data_management.py
@@ -0,0 +1,11 @@
+""""""
+__author__ = "Casper Kristiansson"
+__copyright__ = "WeatherBrain"
+
+__maintainer__ = "Casper Kristiansson"
+__email__ = "casper@pktraffic.com"
+__status__ = "Development"
+
+class DataManagement:
+    def __init__(self):
+        pass
