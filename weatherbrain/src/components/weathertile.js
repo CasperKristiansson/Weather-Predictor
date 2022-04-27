99a1c1209d21df7d4f4580584d115e9dad699cfc
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-04-27 23:19:03 +0200

    testing

---
+++ weatherbrain/src/components/weathertile.js
@@ -0,0 +1,9 @@
+export const WeatherTile = () => {
+    return (
+        <div>
+            22 grader lixom
+            
+            
+        </div>
+    );
+}
---
+++ weatherbrain/src/view/startpage.js
@@ -0,0 +1,14 @@
+import { WeatherTile } from "../components/weathertile";
+
+
+export const Startpage = () => {
+    return (
+        <>
+        
+            <h1>WeatherBrain</h1>
+            <p>A simple weather app</p>
+            <WeatherTile/>
+
+        </>
+    );
+}
