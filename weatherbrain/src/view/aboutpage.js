dabbb82584a1e001b33f73d16dddba980b9f7eb4
VIIILLE <vvik@kth.se>
2022-04-28 15:57:07 +0200

    created initial files for the about us pages

---
+++ weatherbrain/src/view/aboutpage.js
@@ -0,0 +1,11 @@
+import '../styling/pages/aboutpage.css';
+
+export const Aboutpage = () => {
+    return (
+        <>
+            <h1>WeatherBrain</h1>
+            <p>A simple weather app</p>
+            
+        </>
+    );
+}
