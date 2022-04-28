775e53ed59f0f237762684c0af6e8a9bc6e37196
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-04-28 11:25:32 +0200

    testing out components

--- weatherbrain/src/components/weathertile.js
+++ weatherbrain/src/components/weathertile.js
@@ -1,9 +1,15 @@
 export const WeatherTile = () => {
     return (
         <div>
-            22 grader lixom
-            
-            
+            <p>22°C</p> 
+            <div>
+            <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+            <span class="material-symbols-outlined">
+            sunny
+            </span>
+            </div>
         </div>
+        
     );
 }
+
