1cc2f433b811752ea894881c811f0be3e1322b32
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-04-28 12:00:02 +0200

    experimental css

--- weatherbrain/src/components/weathertile.js
+++ weatherbrain/src/components/weathertile.js
@@ -1,12 +1,16 @@
+import '../styling/components/weathertile.css';
+
 export const WeatherTile = () => {
     return (
-        <div>
+        <div class="tilediv">
+            <div class="content-holder">
             <p>22°C</p> 
             <div>
             <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
             <span class="material-symbols-outlined">
             sunny
             </span>
+            </div>
             </div>
         </div>
         
