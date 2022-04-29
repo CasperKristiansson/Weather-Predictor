782eda7f0d1af4590d432a269ee611881688d91c
Fredrik J <71126241+janetzky@users.noreply.github.com>
2022-04-29 12:46:10 +0200

    Removed some unnecessary stuff

--- weatherbrain/src/pages/about.js
+++ weatherbrain/src/pages/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 const About = () => {
-        console.log("Hejs");
     return (
         <div>
             <p>This is the magical project group eight that will rock the maddafakking weather from now on.</p>
