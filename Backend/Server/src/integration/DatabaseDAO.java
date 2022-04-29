a84715e64a5c38c1ef709e2b82ed85ad27d165e7
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-04-29 14:08:31 +0200

    commit

--- Backend/Server/src/integration/DatabaseDAO.java
+++ Backend/Server/src/integration/DatabaseDAO.java
@@ -1,25 +1,30 @@
 package integration;
+
 import java.io.*;
 import java.sql.*;
 import java.util.*;
+
+import java.io.File;
+import java.util.Scanner;
 
 public class DatabaseDAO {
     private Connection connection;
 
     private static final String WEATHER_DATA_TABLE_NAME = "smhiData";
-    
+    private static final String WEATHER_DATA_DATE_COLUMN_NAME = "date";
+    private static final String WEATHER_DATA_TEMPERATURE_COLUMN_NAME = "temperature";
+    private static final String WEATHER_DATA_AIR_PRESSURE_COLUMN_NAME = "airPressure";
 
-
-    public DatabaseDAO(){
-        try{
+    public DatabaseDAO() {
+        try {
             connectToDB();
             prepareStatements();
-        }catch(SQLException | IOException e){
+        } catch (SQLException | IOException e) {
             System.out.println("Error:  " + e.getMessage());
         }
-    } 
+    }
 
-    private void connectToDB() throws SQLException, IOException{
+    private void connectToDB() throws SQLException, IOException {
         InputStream input = new FileInputStream("src/credentials/databaseCredentials.properties");
         Properties prop = new Properties();
         prop.load(input);
@@ -27,18 +32,25 @@ public class DatabaseDAO {
         String connectionString = prop.getProperty("connectionString");
         this.connection = DriverManager.getConnection(connectionString);
         System.out.println("Connected to database");
-        this.connection.close();
     }
 
-    private void insertWeatherData(String[] date, String[] time, int[] tempereture, int[] airPressure){
-
+    public void insertWeatherData() {
+        File file = new File("src/data/smhi_data.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                PreparedStatement query = connection.prepareStatement(scanner.nextLine());
+                query.executeUpdate();
+                connection.commit();
+            }
+            System.out.println("DONE");
+            scanner.close();
+        } catch (FileNotFoundException | SQLException e) {
+            System.out.println("Error:  " + e.getMessage());
+        }
     }
 
-    private void prepareStatements() throws SQLException{
+    private void prepareStatements() throws SQLException {
         PreparedStatement getAll = connection.prepareStatement("SELECT * FROM ?");
-    }
-
-    public void insertWeatherData(){
-
     }
 }
