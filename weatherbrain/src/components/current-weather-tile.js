36970652c3152e49d142a2925a478fab4377dbd3
phhagg <62904448+huzzleproof@users.noreply.github.com>
2022-04-29 11:45:44 +0200

    sync

--- weatherbrain/src/components/current-weather-tile.js
+++
@@ -1,9 +0,0 @@
-import '../styling/components/currentweathertile.css'
-
-export const CurrentWeatherTile = () => {
-    return (
-        <div class="container">
-            
-        </div>
-    );
-}
