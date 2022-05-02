94a03b5f49945e8d4e66e96927bcc61328f842d4
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-05-02 11:20:29 +0200

    Delete ManualUpload.java

--- Backend/Server/src/main/ManualUpload.java
+++
@@ -1,8 +0,0 @@
-package main;
-import controller.Database;
-
-public class ManualUpload {
-    public static void main(String[] args) throws Exception {
-        Database db = new Database();
-    }
-}
