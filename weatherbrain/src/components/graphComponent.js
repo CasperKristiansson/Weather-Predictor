7ba4b4808e42c844d4d0baac0c3e3f5acdafec13
Chouster-KTH <chouster@kth.se>
2022-05-04 21:29:54 +0200

    Changed graph

--- weatherbrain/src/components/graphComponent.js
+++ weatherbrain/src/components/graphComponent.js
@@ -1,5 +1,4 @@
-import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar, LabelList } from 'recharts';
 import '../styling/components/plotGraph.css'
 
 
@@ -7,89 +6,72 @@ import '../styling/components/plotGraph.css'
 const data = [
   {
     name: 'Mon',
-    // uv: 4000,
     temperature: 22,
-    amt: 2400,
   },
   {
     name: 'Tue',
-    // uv: 3000,
     temperature: 22,
-    amt: 2210,
   },
   {
     name: 'Wed',
-    // uv: 2000,
     temperature: 14,
-    amt: 2290,
   },
   {
     name: 'Thur',
-    // uv: 2780,
     temperature: 22,
-    amt: 2000,
   },
   {
     name: 'Fri',
-    // uv: 1890,
     temperature: 22,
-    amt: 2181,
   },
   {
     name: 'Sat',
-    // uv: 2390,
     temperature: 14,
-    amt: 2500,
   },
   {
     name: 'Sun',
-    // uv: 3490,
     temperature: 18,
-    amt: 2100,
   },
 ];
 
-// export default class Example extends PureComponent {
-  // static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
+export const PlotGraph = () => {
+  return (
+    <div className="theGraph">
 
-  // render() {
-    export const PlotGraph = () =>
-    {
-    return (
-      <div className = "theGraph" 
-      >
-
-         {/* <ResponsiveContainer  */}
-        {/* //  width="500" height="300" */}
-         {/* >  */}
-        <LineChart
+      <LineChart
         width={1000}
         height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-            <YAxis label="°C" />
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <YAxis label={{ value: '°C', fill: 'rgb(129, 106, 235)', angle: 0, position: 'insideLeft' }} />
 
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis 
-          dataKey="name" 
-          />
-          <YAxis name = "Temperature (°C)" />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" 
-          dataKey="temperature" 
-          stroke="#8884d8" activeDot={{ r: 8 }} 
-        //   name = "Day of week" 
-          />
-          {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
-        </LineChart>
-        {/* </ResponsiveContainer> */}
-      </div>
-    );
-  }
+        <Bar dataKey="temperature">
+          <LabelList dataKey="name" position="insideTopLeft" />
+        </Bar>
+
+        {/* <YAxis label={{ value: "Temperature (°C)", dy: 10, angle: -90,  }} /> */}
+
+        {/* <XAxis dataKey="name" label={{ value: "Day of week", position: "insideBottomLeft", dy: 10}} /> */}
+
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="name"
+        />
+        <YAxis name="Temperature (°C)" />
+        <Tooltip />
+        <Legend />
+
+        <Line type="monotone"
+          dataKey="temperature"
+          stroke="#8884d8" activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    </div>
+  );
+}
