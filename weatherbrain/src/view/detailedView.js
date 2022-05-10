7a1e0ce73892b78288d63472bda6bfe075607a5b
Chouster-KTH <chouster@kth.se>
2022-05-10 14:56:44 +0200

    Started working on stat page

--- weatherbrain/src/view/detailedView.js
+++ weatherbrain/src/view/detailedView.js
@@ -1,11 +1,46 @@
-import { Graph } from "../components/plotGraph";
+//import { Graph } from "../components/plotGraph";
 
+//Average
+//Humidity
+//statistics
+//Highest
+//History, last week?
+
+
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var currentDate = new Date();
+var startDate = new Date(currentDate.getFullYear(), 0, 1);
+var days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+let nameOfMonth = month[currentDate.getMonth()];
+var weekNumber = Math.ceil((currentDate.getDay() + 1 + days) / 7);
+let year = startDate.getYear();
 
 export const DetailedView = () => {
     return (
         <div>
-            <Graph />
+            <h2>Statistics for current week</h2>
+
+            Average temperature: <br></br>
+            Lowest temperature: <br></br>
+            Highest temperature: <br></br>
+
+            <h2>Statistics for last week (week number {weekNumber - 1})</h2>
+
+            Average temperature: <br></br>
+            Lowest temperature: <br></br>
+            Highest temperature: <br></br>
+
+            <h2>Statistics for {nameOfMonth} {1900 + year}</h2>
+
+            Average temperature: <br></br>
+            Lowest temperature: <br></br>
+            Highest temperature: <br></br>
         </div>
+
+
+        // <div>
+        //     <Graph />
+        // </div>
     );
 
 }
