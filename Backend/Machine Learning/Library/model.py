148a2e6e01dab9ba40f698b398663ec3061d1a3f
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-05-12 11:51:42 +0200

    Add comments to running model Predictions

--- Backend/Machine Learning/Library/model.py
+++ Backend/Machine Learning/Library/model.py
@@ -1,6 +1,6 @@
-import os
 import numpy as np
 import datetime
+import time
 
 import tensorflow as tf
 from tensorflow import keras
@@ -9,17 +9,26 @@ from tensorflow.keras.backend import square, mean
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class Model:
     def __init__(self):
         self.model = None
         self.df = None
     
     def load_model(self, file_name):
+        '''
+        Method for loading a previously trained model.
+
+        :param file_name: Name of the file to load.
+
+        :return: None
+        '''
         def loss_mse_warmup(y_true, y_pred):
             warmup_steps = 50
             """
             Calculate the Mean Squared Error between y_true and y_pred,
             but ignore the beginning "warmup" part of the sequences.
+            Custom loss function for use with keras.
             
             y_true is the desired output.
             y_pred is the model's output.
@@ -28,13 +37,24 @@ class Model:
             y_true_slice = y_true[:, warmup_steps:, :]
             y_pred_slice = y_pred[:, warmup_steps:, :]
 
-            mse = mean(square(y_true_slice - y_pred_slice))
-            
-            return mse
+            return mean(square(y_true_slice - y_pred_slice))
 
         self.model = keras.models.load_model(file_name, custom_objects={'loss_mse_warmup': loss_mse_warmup})
     
     def load_old_data(self):
+        '''
+        Loads the old data to use to predict the future weather data
+
+        :return: None
+
+        For testing sake you can drop data for a specific period
+        To see how the prediction gets affected by it.
+        200000 -> 50000 = 70% Worse Prediction
+        200000 -> 100000 = 30% Worse Prediction
+        
+        The time for predicting is pretty much the same regardless of the
+        Input width.
+        '''
         self.df = pd.read_csv('..\\Data\\Processed Data\\smhi_data.csv', low_memory=False)
         self.df['Date'] = pd.to_datetime(self.df['Date'], format='%Y-%m-%d %H:%M:%S')
         self.df['Day'] = self.df['Date'].dt.day_of_year
@@ -45,12 +65,10 @@ class Model:
 
         # self.df = self.df.drop(self.df.index[:150000])
 
-        # Remove the first 70% of the data
-
-    def save_model(self, file_name):
-        self.model.save(file_name)
-
     def example(self):
+        '''
+        Example for plotting previous predictions.
+        '''
         def plot_comparison(start_idx, length=100, train=True):
             """
             Plot the predicted and true output-signals.
@@ -118,15 +136,11 @@ class Model:
 
         df_targets = df[['Temperature', 'Air Pressure', 'Humidity']]
 
-        print(df_targets.tail())
-
         x_data = df.values[:-(24 * 7)]
         y_data = df_targets.values[:-(24 * 7)]
 
         x_train = x_data[:191903]
         x_test = x_data[191903:]
-
-        print(x_test)
 
         y_train = y_data[:191903]
         y_test = y_data[191903:]
@@ -139,12 +153,20 @@ class Model:
         y_train_scaled = y_scaler.fit_transform(y_train)
         y_test_scaled = y_scaler.transform(y_test)
 
-        print(y_test_scaled)
-
         plot_comparison(start_idx=0, length=(24*7), train=True)
 
     
-    def get_value(self, period):
+    def get_prediction(self, period):
+        '''
+        The method will return the predicted weather data for the next period.
+        The period is specified in hours in the future. See "Time Series Prediction"
+        notebook for a bit more information on how the data is transformed so the
+        neural network can work with it.
+
+        :param period: The period in hours in the future.
+
+        :return: The predicted weather data.
+        '''
         df_targets = self.df[['Temperature', 'Air Pressure', 'Humidity']].shift(-period)
 
         x_data = self.df.values[:-period]
@@ -161,19 +183,28 @@ class Model:
 
         y_pred_rescaled = y_scaler.inverse_transform(y_pred[0])
 
-        return y_pred_rescaled[:, 0][-1]
+        return y_pred_rescaled[:, 0][-1], y_pred_rescaled[:, 1][-1], y_pred_rescaled[:, 2][-1]
 
 if __name__ == '__main__':
     model = Model()
     model.load_model('saved_model\\weather_model')
     model.load_old_data()
 
-    period = 24 * 7
+    result_df = pd.DataFrame(columns=['Date', 'Temperature', 'Air Pressure', 'Humidity'])
+    start_date = datetime.datetime.now()
+    period = 24 * 7     # The amount of steps to predict in the future.
 
+    start_time = time.time()
+    for i in range(1, period + 1):
+        prediction = model.get_prediction(i)
+        result_df = result_df.append({
+            'Date': start_date + datetime.timedelta(hours=i),
+            'Temperature': prediction[0],
+            'Air Pressure': prediction[1],
+            'Humidity': prediction[2]
+        }, ignore_index=True)
 
-    date = model.df.tail(1).index
-    print(date)
-    print(date + datetime.timedelta(hours=period))
+        print(i, {'Temperature': prediction[0], 'Air Pressure': prediction[1], 'Humidity': prediction[2]}, time.time() - start_time)
+        break
 
-    print(model.get_value(period))
-    # model.get_desired_dataframe()
+    # result_df.to_csv('prediction.csv')
