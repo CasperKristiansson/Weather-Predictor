35f98145ce5d32db9655ac02e42838585ef8f51b
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-05-12 11:26:49 +0200

    test seems to be working
    Adding some URL's for fetching data

---
+++ weatherbrain/src/tests/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from '../App.js';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDom.render(<App/>, div);
+    ReactDom.unmountComponentAtNode(div);
+});
