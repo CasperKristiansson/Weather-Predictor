b3c009b998024668393df7046f5c9003d6ec2e2c
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-05-13 14:17:04 +0200

    fixed some tests

--- weatherbrain/src/tests/currentWeatherTileHolder.test.js
+++ weatherbrain/src/tests/currentWeatherTileHolder.test.js
@@ -1,12 +1,10 @@
 import CurrentWeatherTileHolder from "../components/currentWeatherTile";
 
 import React from 'react';
-import ReactDom from 'react-dom';
-
 import {render, cleanup} from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 
-import renderer from 'react-test-renderer';
+
 
 afterEach(cleanup);
 
