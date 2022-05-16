2a2277bef8be9137bd8b06ad781be9feee218944
Fredrik J <71126241+janetzky@users.noreply.github.com>
2022-05-16 11:54:24 +0200

    updated the contactform
    done

--- weatherbrain/src/view/contactUs.js
+++ weatherbrain/src/view/contactUs.js
@@ -1,45 +1,67 @@
 import React, { useState } from "react";
+import emailjs from 'emailjs-com';
 import '../styling/components/contactUs.css';
-
-const FORM_ENDPOINT = ""; // TODO - fill on the later step
+import emailpic from '../img/emailpic.png';
 
 const ContactForm = () => {
-    const [submitted, setSubmitted] = useState(false);
-    const handleSubmit = () => {
-        setTimeout(() => {
-            setSubmitted(true);
-        }, 100);
-    };
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
 
-    if (submitted) {
+    const submit = () => {
+        if (name && email && message) {
+            const serviceId = 'service_id';
+            const templateId = 'template_id';
+            const userId = 'user_id';
+            const templateParams = {
+                name,
+                email,
+                message
+            };
+
+            emailjs.send(serviceId, templateId, templateParams, userId)
+                .then(response => console.log(response))
+                .then(error => console.log(error));
+
+            setName('');
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);
+        } 
+    }
+    if (emailSent) {
         return (
-            <div className='textContainer'>
-                <h1>Thank you!</h1>
-                <p>We'll be in touch soon.</p>
+            <div>
+                <div className='textContainer'> 
+                    <h1>Thank you!</h1>
+                    <img src={emailpic} />
+                    <p>We'll be in touch soon.</p>
+                </div>
             </div>
         );
     }
 
     return (
-        <form
-            action={FORM_ENDPOINT}
-            onSubmit={handleSubmit}
-            method="POST"
-            target="_blank"
+        <div 
+        id="contact-form"
         >
             <div className="margin">
-                <input type="text" placeholder="Your name" name="name" required  />
+                <input type="text" placeholder="Your name" name="name" required 
+                value={name} onChange={e => setName(e.target.value)}  />
             </div>
             <div>
-                <input type="email" placeholder="Email" name="email" required />
+                <input type="email" placeholder="Email" name="email" required
+                value={email} onChange={e => setEmail(e.target.value)} />
             </div>
             <div>
-                <textarea placeholder="Your message" name="message" required />
+                <textarea placeholder="Your message" name="message" required
+                value={message} onChange={e => setMessage(e.target.value)} />
             </div>
             <div>
-                <button type="submit" className='button'> Send a message </button>
+                <button type="submit" className='button' onClick={submit}> Send a message </button>
             </div>
-        </form>
+        </div>
     );
 };
 
