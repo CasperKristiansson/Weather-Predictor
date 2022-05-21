f8c5e0befc99ad0e40768445beb9ee054d1a3056
Casper kristiansson <Casper.kristiansson@yahoo.se>
2022-05-21 16:16:31 +0200

    Display none on mobile view

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 import Home from './Home/home';
 
 function App() {
+
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <div className="App">
+        <Home />
+      </div>
+      <div className="displayMobile">
+        <h1>Content Not Design For Mobile (less than 1000px), please view on Desktop</h1>
+      </div>
+    </>
   );
 }
 
--- src/Home/home.js
+++ src/Home/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './home.css'
 import background from './6317024.jpg'
 import CurrentWeatherDisplay from './currentWeatherDisplay'
@@ -6,11 +6,12 @@ import UpcomingWeatherDisplay from './upcomingWeatherDisplay'
 import Navigation from './navigation'
 
 export default () => {
+
   return (
     <>
       <div className="FullPageBackground" style={{ backgroundImage: `url(${background})` }}/>
-
       <Navigation />
+
       <div className="weatherDisplay">
         <CurrentWeatherDisplay />
         <UpcomingWeatherDisplay />
